2e38c4ee75648fbe12a031b4664d0d7967c2d8cf
Chetan Gadgil <c.a.gadgil@gmail.com>
2013-05-04 07:16:50 -0700

    Basic working formula with Black/Scholes

---
+++ openoptions/app/net/gadgil/options/pricing/BlackScholes.scala
@@ -0,0 +1,46 @@
+package net.gadgil.options.pricing
+
+import org.apache.commons.math3.distribution.NormalDistribution
+
+class BlackScholes {
+  def callOption(stockPrice: Double,
+    strikePrice: Double,
+    timeToExpiry: Double,
+    riskFreeRate: Double,
+    volatility: Double) = {
+    val d1 = (math.log(stockPrice / strikePrice) +
+      (riskFreeRate + math.pow(volatility, 2) / 2) * timeToExpiry) / (volatility * math.sqrt(timeToExpiry))
+    //Console.println(d1)
+    val d2 = d1 - volatility * math.sqrt(timeToExpiry)
+    val nd1 = CND(d1)
+    val nd2 = CND(d2)
+    //Console.println(nd1, nd2)
+    stockPrice * nd1 -
+      strikePrice * math.exp(-riskFreeRate * timeToExpiry) * nd2
+  }
+  
+  def CND(X: Double) = {
+        val theNormalDistribution = new NormalDistribution
+        theNormalDistribution.cumulativeProbability(X)
+  }
+
+  // Approximation of the cumulative normal distribution function 
+  def CNDApprox(X: Double) = {
+    val a1 = 0.31938153
+    val a2 = -0.356563782
+    val a3 = 1.781477937
+    val a4 = -1.821255978
+    val a5 = 1.330274429
+
+    val L = Math.abs(X);
+    val K = 1.0 / (1.0 + 0.2316419 * L);
+    var w = 1.0 - 1.0 / Math.sqrt(2.0 * Math.PI) * Math.exp(-L * L / 2) * (a1 * K + a2 * K * K + a3
+      * Math.pow(K, 3) + a4 * Math.pow(K, 4) + a5 * Math.pow(K, 5))
+
+    if (X < 0.0) {
+      w = 1.0 - w
+    }
+    w
+  }
+
+}
