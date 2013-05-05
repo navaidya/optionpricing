87266651fbd585faf030e1e323b4a5e996bf328a
Chetan Gadgil <c.a.gadgil@gmail.com>
2013-05-05 11:12:37 -0700

    added stock price discount by dividend payouts

--- cgoptpricing/src/main/scala/net/gadgil/cgoptprice/PlainVanilla.scala
+++ cgoptpricing/src/main/scala/net/gadgil/cgoptprice/PlainVanilla.scala
@@ -1,5 +1,16 @@
 package net.gadgil.cgoptprice
 
-class PlainVanilla {
-	
+object PlainVanilla {
+  /**
+   * Calculates the adjusted stock price after dividend deduction
+   * @param dividends - list of tuples containing the dividend time and value respectively 
+   */
+	def adjustedStockPriceAfterDividendDeduction(stockPrice: Double, riskFreeRate: Double, dividends: List[(Double, Double)]) = {
+	  val theDiscountedDividends = dividends.map { theDividend =>
+	    math.exp(-riskFreeRate * theDividend._1) * theDividend._2
+	  }
+	  theDiscountedDividends.foldLeft(stockPrice) { (theTotal, theValue) =>
+	    theTotal - theValue
+	  }
+	}
 }
