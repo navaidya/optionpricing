749167174e03c7db6199ffdeb7cf7fd47af6aa34
Chetan Gadgil <c.a.gadgil@gmail.com>
2013-05-18 18:01:30 -0700

    updated testcase

--- finance.portfolio/src/main/scala/net/gadgil/finance/portfolio/PortfolioDefinition.scala
+++ finance.portfolio/src/main/scala/net/gadgil/finance/portfolio/PortfolioDefinition.scala
@@ -9,27 +9,35 @@ class PortfolioDefinition {
 }
 
 object PortfolioDefinition extends JavaTokenParsers {
-  def portfolioStruct: Parser[Any] = "portfolio" ~> ident ~ portfolioComposition ^^ {
+  def portfolioStruct: Parser[PortfolioInfo] = "portfolio" ~> ident ~ portfolioComposition ^^ {
     case portfolioName ~ portfolioComposition =>
-      ""
+      PortfolioInfo(portfolioName, portfolioComposition)
   }
-  def portfolioComposition: Parser[Any] = position.+
-  def position: Parser[Any] = ("long" | "short") ~ ident ~ "$" ~ decimalNumber ~ onDate1 ^^ {
-    case positionType ~ symbol ~ dollars ~ amount ~ theDate => { println(positionType, symbol, amount, theDate) }
+  def portfolioComposition: Parser[Seq[PortfolioPosition]] = position.+
+  def position: Parser[PortfolioPosition] = ("long" | "short") ~ ident ~ "$" ~ decimalNumber ~ onDate1 ^^ {
+    case positionType ~ symbol ~ dollars ~ amount ~ theDate => {
+      //println(positionType, symbol, amount, theDate)
+      PortfolioPosition(positionType, symbol, amount.toDouble, theDate)
+    }
   }
   def onDate1: Parser[DateTime] = new Regex("([0-9]{4})-([0-9]{2})-([0-9]{2})") ^^ { theDate =>
     val r = new Regex("([0-9]{4})-([0-9]{2})-([0-9]{2})")
     val y = List("DD")
-    val theMatchedGroups = r.findFirstMatchIn(theDate).fold(y){xxx => xxx.subgroups}
-    //println(x.toList)
-    //nim
-    //theMatchedGroups
-    //val x = new DateTime()
-    new DateTime(theMatchedGroups(0).toInt, theMatchedGroups(1).toInt, theMatchedGroups(2).toInt, 0,0 )
-    //x
+    val theMatchedGroups = r.findFirstMatchIn(theDate).fold(y) { xxx => xxx.subgroups }
+    new DateTime(theMatchedGroups(0).toInt, theMatchedGroups(1).toInt, theMatchedGroups(2).toInt, 0, 0)
   }
 }
 
 case class TradeDate(year: Int, month: Int, day: Int)
 case class PortfolioInfo(name: String, positions: Seq[PortfolioPosition])
-case class PortfolioPosition(symbol: String, numShares: Double)
+case class PortfolioPosition(positionType: String, symbol: String, amount: Double, dateTime: DateTime)
+
+object PortfolioProcessor {
+  def getHistoricalPrices(symbol: String, fromDate: DateTime, toDate: DateTime = DateTime.now()) = {
+    
+  }
+  
+  def parsePortfolio(portfolioDefinitionString: String): PortfolioInfo = {
+    PortfolioDefinition.parseAll(PortfolioDefinition.portfolioStruct, portfolioDefinitionString).get
+  }
+}
--- finance.portfolio/src/test/scala/PortfolioTools.scala
+++ finance.portfolio/src/test/scala/PortfolioTools.scala
@@ -2,18 +2,20 @@ package net.gadgil.finance.portfolio
 
 import org.specs2.mutable._
 
-class StubSpec extends Specification {
+class PortfolioToolsSpecification extends Specification {
 
   "portfolio parser" should {
     val theDefinition = """
       portfolio pf1
-      long CSCO $ 100.3 2008-02-02
-      long MSFT $ 22.3  2008-02-02
-      short GE $ 15.3 2008-02-02
+      long CSCO $ 1000 2008-02-02
+      long MSFT $ 1000  2008-02-02
+      short GE $ 1000 2008-02-02
         """
     "parse test" in {
       val x = PortfolioDefinition.parseAll(PortfolioDefinition.portfolioStruct, theDefinition)
       println(x)
+      x.get.name must be equalTo "pf1"
+      x.get.positions(0).symbol must be equalTo "CSCO"
     }
   }
 }
