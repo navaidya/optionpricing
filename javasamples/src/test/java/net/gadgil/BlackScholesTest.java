e40041d19ca3f5085776fbeded5c59d735e4be34
Naval Vaidya <navalvaidya@gmail.com>
2013-05-25 18:30:10 +0530

    added two more tests for black scholes call and put

--- javasamples/src/test/java/net/gadgil/BlackScholesTest.java
+++ javasamples/src/test/java/net/gadgil/BlackScholesTest.java
@@ -3,6 +3,7 @@ package net.gadgil;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import net.gadgil.cgoptprice.BlackScholes;
 
 public class BlackScholesTest extends TestCase {
 	/**
@@ -28,7 +29,8 @@ public class BlackScholesTest extends TestCase {
 	 */
 	public void testBlackScholesGeneralizedCallOption()
 	{
-		double optionValue = net.gadgil.cgoptprice.BlackScholes.callOptionGeneralizedBlackScholes(60, 65, 0.25, 0.08, 0.08, 0.3);
+		double optionValue = BlackScholes.callOptionGeneralizedBlackScholes(
+				60, 65, 0.25, 0.08, 0.08, 0.3);
 		double diff = Math.abs(2.1334 - optionValue);
 		if(diff < 0.0001)
 			assertTrue( true );
@@ -38,8 +40,31 @@ public class BlackScholesTest extends TestCase {
 	
 	public void testBlackScholesGeneralizedPutOption()
 	{
-		double optionValue = net.gadgil.cgoptprice.BlackScholes.putOptionGeneralizedBlackScholes(75, 70, 0.5, 0.1, 0.05, 0.35);
+		double optionValue = BlackScholes.putOptionGeneralizedBlackScholes(
+				75, 70, 0.5, 0.1, 0.05, 0.35);
 		double diff = Math.abs(4.0870 - optionValue);
+		if(diff < 0.0001)
+			assertTrue( true );
+		else 
+			assertTrue(false);
+	}
+	
+	public void testBlackScholesCallOption()
+	{
+		double optionValue = BlackScholes.callOption(
+				60, 65, 0.25, 0.08, 0.3);
+		double diff = Math.abs(2.1334 - optionValue);
+		if(diff < 0.0001)
+			assertTrue( true );
+		else 
+			assertTrue(false);
+	}
+	
+	public void testBlackScholesPutOption()
+	{
+		double optionValue = BlackScholes.putOption(
+				65, 60, 0.25, 0.08, 0.3);
+		double diff = Math.abs(1.39866 - optionValue);
 		if(diff < 0.0001)
 			assertTrue( true );
 		else 
